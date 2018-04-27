da6b9d1a9d1004bc0dd5959c3c798d1998fa4e77
Mat Ramseier <matthias.ramseier@ict-campus.net>
2018-04-27 14:28:40 +0200

    Spiellogik und spiel Klasse implementiert

---
+++ actual project/src/net/ictcampus/rts/model/Spiel.java
@@ -0,0 +1,21 @@
+package net.ictcampus.rts.model;
+
+import java.util.ArrayList;
+
+public class Spiel {
+    private Spielfeld spielfeld;
+    private ArrayListy<Player> spieler;
+    
+    public Spiel(int xLength, int yLength, Player... spieler ) {
+        
+    }
+    
+    
+    public boolean commandAusfuehren(int , int ) {
+        
+    }
+    
+    public void spielerHinzufuegen(Player spieler) {
+        
+    }
+}
---
+++ actual project/src/net/ictcampus/rts/model/SpielLogik.java
@@ -0,0 +1,13 @@
+package net.ictcampus.rts.model;
+
+import java.util.ArrayList;
+
+public class SpielLogik {
+    private Spiel spiel;
+    private SpielFeld feld;
+    private ArrayList<Player> spieler;
+    
+    public SpielLogik(ArrayList<Player>) {
+        
+    }
+}
