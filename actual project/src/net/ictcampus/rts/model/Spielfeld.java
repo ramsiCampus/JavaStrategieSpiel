6f259534fceb96b82f7a10373629cc823b1b251b
Mat Ramseier <matthias.ramseier@ict-campus.net>
2018-04-27 16:32:45 +0200

    add class spielfeld

---
+++ actual project/src/net/ictcampus/rts/model/Spielfeld.java
@@ -0,0 +1,20 @@
+package net.ictcampus.rts.model;
+
+public class Spielfeld {
+
+    private List<Mensch> einheiten;
+    private List<Stadt> staedte;
+    private Feld[][] felder;
+    
+    public Spielfeld(int xLength, int yLength) {
+        
+    }
+    
+    public void stadtBauen(int xPos, int yPos, Player spieler, int int1) {
+        
+    }
+    
+    public void menschBewegen(int xPos, int yPos, Mensch mensch) {
+        
+    }
+}
