c973d65900161d91ae012b494a2b84931af2dcd8
Johanna Koch <johanna.koch@ict-campus.net>
2018-05-04 11:19:01 +0200

    ClientSpielController waitfunktion feddisch

--- actual project/src/net/ictcampus/rts/controller/ClientSpielController.java
+++ actual project/src/net/ictcampus/rts/controller/ClientSpielController.java
@@ -4,6 +4,7 @@ import net.ictcampus.rts.view.Testframe;
 
 /**
  * ClientSpielController
+ * 
  * @author kochjo
  * @version 1.0
  */
@@ -13,20 +14,29 @@ public class ClientSpielController {
     Testframe tF;
     Boolean ready = false;
     String command;
-    
+
     // -------------------------------Constructor--------------------------------//
 
     // -----------------------------------Main-----------------------------------//
 
     // ---------------------------------Methods---------------------------------//
-    public void waitForInput() {
-        while(!ready) {
+    /**
+     * waitForCommand() wartet auf die Befehle vom GUI und sendet diese
+     * anschliessend an den Server weiter
+     */
+    public void waitForCommand() {
+        while (!ready) {
             command = tF.getCommand();
             ready = tF.getReady();
             if (ready) {
+                ready = false;
                 break;
             }
         }
+        cltCtrl.sendCommand(command);
+    }
+    
+    public void getPlayerID() {
         
     }
 }
